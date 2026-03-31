5500d372b10ad108e52cebe2dbac9308814a6ccf
shengjunpeng <1300749625@qq.com>
2026-03-31 14:34:28 +0800

    feat: 清空

--- apps/web/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import reactConfig from "@agent-mono/eslint-config/react";
-
-/** @type {import("eslint").Linter.Config[]} */
-export default [...reactConfig];
--- apps/web/src/pages/prompt-playground/index.ts
+++
@@ -1,1 +0,0 @@
-export { PromptPlaygroundPage } from "./PromptPlaygroundPage";
